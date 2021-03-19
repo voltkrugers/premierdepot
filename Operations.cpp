1b475773d1fffa993f47240b8d680e67d9669871
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2021-03-19 09:48:38 +0100

    exposant
    on a rajouter le fonction exp()

--- Operations.cpp
+++ Operations.cpp
@@ -1,4 +1,5 @@
 #include "Operations.h"
+#include <math.h>  
 
 int addition(int a, int b)
 {
@@ -15,5 +16,24 @@ int soustraction(int a, int b)
 	
 	resultat = a - b;
 	
+	return resultat;
+}
+int multiplication(int a, int b)
+{
+	int resultat;
+	resultat=a * b;
+	return resultat;
+	
+}
+int division(int a ,int b)
+{
+	int resultat;
+	resultat=a/b;
+	return resultat;
+}
+int exposant(int nb, int exp)
+{
+	int resultat;
+	resultat=exp(np);
 	return resultat;
 }
--- Operations.h
+++ Operations.h
@@ -1,3 +1,9 @@
 int addition(int a, int b);
 
 int soustraction(int a, int b);
+
+int multiplication(int a, int b);
+
+int division(int a ,int b);
+
+int exposant(int nb, int exp);
