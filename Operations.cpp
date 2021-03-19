36948f53d18941eb3183d65fea39e5ddcda6a0b3
vduris <80945899+vduris@users.noreply.github.com>
2021-03-19 09:46:38 +0100

    rajout fct

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,21 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+
+int multiplication(int a, int b)
+{
+	int resultat;
+
+	resultat = a * b;
+
+	return resultat;
+}
+
+float division(int a, int b)
+{
+	float resultat;
+
+	resultat = a / b;
+
+	return resultat;
+}
